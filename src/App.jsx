c2acc328b2f3e33d056898145922d1816dd07c2a
Vidit <viditgoel2k@gmail.com>
2023-10-15 20:28:34 +0530

    setup of app component for using redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react'
 import './App.css'
 
+import authService from './appwrite/auth_service';
+
+import { useDispatch } from 'react-redux'
+import {login, logout} from './store/authSlice'
+
+import { Outlet } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
 function App() {
-  return (
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+
+      return loading ? (null) : (
+        <div></div>
+      )
+
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return loading ? (null) : (
     <>
-      <h1>Mega blog app with appwrite</h1>
+      <div className='flex flex-wrap content-between min-h-screen bg-gray-400 ' >
+        <div className='block w-full ' >
+          <Header />
+          <main>
+            {/* //TODO: <Outlet /> */}
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
